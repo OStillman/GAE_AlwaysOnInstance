dfd650945467e31d89d0b8504d0ff3759f6f354d
OStillman <owenstillman@live.co.uk>
2018-10-09 13:03:54 +0100

    Shutdown Defined

--- counter.py
+++ counter.py
@@ -5,3 +5,11 @@ import webapp2
 from google.appengine.api import runtime
 from google.appengine.api import background_thread
 
+GLOBAL_COUNTER = 1
+
+class Shutdown(Exception):
+    pass
+
+def shutdown():
+    logging.info("Shutdown hook called")
+    raise Shutdown
