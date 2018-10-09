c9e315fa4295688f4d40c30a87de1829151a70d0
OStillman <owenstillman@live.co.uk>
2018-10-09 13:05:44 +0100

    Define Counter_Loop

--- counter.py
+++ counter.py
@@ -13,3 +13,14 @@ class Shutdown(Exception):
 def shutdown():
     logging.info("Shutdown hook called")
     raise Shutdown
+
+def counter_loop():
+    global GLOBAL_COUNTER
+    try:
+        while GLOBAL_COUNTER < 600:
+            GLOBAL_COUNTER += 1
+            time.sleep(1)
+        logging.info("Shutting down after 600 cycles")
+    except:
+        logging.info("Counter loop saw shutdown")
+
